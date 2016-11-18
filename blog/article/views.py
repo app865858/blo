b2c3e870b33c8876f217074bec0d58252cf826ab
ut <app865858@gmail.com>
2016-11-18 11:17:59 +0800

    ch8-2

--- blog/article/views.py
+++ blog/article/views.py
@@ -7,5 +7,11 @@ def article(request):
     Render the article page
     '''
     articles = Article.objects.all()
-    context = {'articles':articles}
+    itemArray = []
+    
+    for article in articles:
+        items = [article]
+        items.extend(list(Comment.objects.filter(article=article)))
+        itemArray.append(items)
+        context = {'itemArray':itemArray}
     return render(request, 'article/article.html', context)
